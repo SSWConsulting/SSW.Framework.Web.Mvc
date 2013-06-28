72cb757688128a92f17cc74ab44c0d293150c5a0
Brendan Richards <brendan@nobadthing.com>
2013-06-28 17:36:05 +1000

    Added instructions.docx

--- SSW.Framework.Web.Mvc4/publish.ps1
+++ SSW.Framework.Web.Mvc4/publish.ps1
@@ -1,6 +1,14 @@
 ﻿
-# pack and publish to nuget
 
+
+
+# get api key
+$p = Split-Path $MyInvocation.MyCommand.Path
+$apiKey = [IO.File]::ReadAllText("$p\Nuget_Api.key")
+
+
+# pack and publish to nuget
 .\Assets\nuget.exe pack .\package\SSW.Framework.Web.Mvc.nuspec
-.\Assets\nuget.exe setApiKey f82b7b27-6972-4e46-b2d6-4adecc6f8c74
+.\Assets\nuget.exe setApiKey $apiKey
 .\Assets\nuget.exe push SSW.Framework.Web.Mvc.1.0.2.nupkg
+
